29c737a49edfe46b82358a09dafab9f7a5f1e7ae
NEERAJ PATHANIA <namoln909@gmail.com>
2023-06-22 00:17:51 +0530

    first commit

---
+++ up-bottom/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [xAxis, setxAxis] = useState(0)
+  const [yAxis, setyAxis] = useState(0)
+
+  return (
+
+    <div className='main_container'>
+      <button className='xAxis' onClick={() => setyAxis(yAxis - 50)} disabled={yAxis === 0}>Up</button>
+      <div className='container'>
+        <button className='yAxis' onClick={() => setxAxis(xAxis - 50)} disabled={xAxis === 0}> Left</button>
+        <div className='main'>
+          <div className='box' style={{ left: xAxis, top: yAxis }}></div>
+          </div>
+          <button className='yAxis' onClick={() => setxAxis(xAxis + 50)} disabled={xAxis === 450}>Right</button>
+        </div>
+        <button className='xAxis' onClick={() => setyAxis(yAxis + 50)} disabled={yAxis === 450}>Bottom</button>
+      </div>
+  );
+}
+
+export default App;
